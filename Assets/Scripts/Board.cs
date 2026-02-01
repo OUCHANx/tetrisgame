efab7ff62ddee0338e4c8f8b983e807f4c6d115e
ouchan <kingbook0628@gmail.com>
2026-02-01 16:18:50 +0900

    テトリスのマスを作った

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,6 +1,40 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Board : MonoBehaviour
 {
+    //やること
+    //変数の作成
+    //ボード基盤用の四角枠格納用
+    //ボードの高さ
+    //ボードのはば
+    //ボードの高さ調整用数値
     
+    [SerializeField]
+    private Transform emptySprite;
+
+    [SerializeField]
+    private int height = 30, width = 10, header = 8;
+
+    void Start()
+    {
+        CreateBoard();
+    }
+    //関数の作成
+    //ボードを生成する関数の作成
+    void CreateBoard()
+    {
+        if (emptySprite)
+        {
+            for (int y = 0; y < height-header; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Transform clone = Instantiate(emptySprite, 
+                    new Vector3(x, y, 0),Quaternion.identity);
+                    clone.transform.parent =  transform;
+                }
+            }
+        }
+    }
 }
